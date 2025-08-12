4b15338e456e861008879306a07b4fad5efd51fc
DeveloperECC <elmercabrerac5@gmail.com>
2025-08-12 16:54:01 +0000

    ultimos cambios

---
+++ mi-pwa-app/android/app/src/main/java/com/developerecc/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.developerecc.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ mi-pwa-app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.developerecc.app',
+  appName: 'palabra_Oculta',
+  webDir: 'build'
+};
+
+export default config;
